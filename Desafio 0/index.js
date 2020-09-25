cdaf27028a0380b356b248edede10ead1f8b1bb3
Rafael Herculano <rafa.lj@hotmail.com>
2020-09-25 09:31:41 -0300

    add desafio 0

---
+++ Desafio 0/index.js
@@ -0,0 +1,1 @@
+function soma(n1, n2) {}
---
+++ Desafio 0/tests.js
@@ -0,0 +1,7 @@
+import { assertEquals } from "../tests.js";
+
+console.log("\n\n\nTESTES:");
+
+assertEquals(soma(1, 2), 3);
+assertEquals(soma(10, 20), 30);
+assertEquals(soma(4, 2), 6);
